8a5fea35a28729ab6e087fd07a52cc058224d0aa
itpark <a.m.smyslov@yandex.ru>
2023-01-04 10:33:52 +0300

    Initial commit

---
+++ ConsoleAppTestProcFunc/Program.cs
@@ -0,0 +1,47 @@
+﻿int InputNumber(string msg, int minValue, int maxValue)
+{
+    bool isCorrectInput;
+    int number = 0;
+    do
+    {
+        isCorrectInput = true;
+
+        try
+        {
+            Console.Write(msg);
+            number = int.Parse(Console.ReadLine());
+
+            if (number < minValue || number > maxValue)
+            {
+                Console.WriteLine($"Ошибка. Число должно быть от {minValue} до {maxValue}");
+                isCorrectInput = false;
+            }
+        }
+        catch
+        {
+            Console.WriteLine("Ошибка. Введите число");
+            isCorrectInput = false;
+        }
+    } while (!isCorrectInput);
+
+    return number;
+}
+
+int Sum(int a, int b)
+{
+    return a + b;
+}
+
+void PrintSum(int sum)
+{
+    Console.WriteLine($"сумма = {sum}");
+}
+
+//--------
+
+int a = InputNumber("введи a (от 1 до 100): ", 1, 100);
+int b = InputNumber("введи b (от 1 до 100): ", 1, 100);
+
+int sum = Sum(a, b);
+
+PrintSum(sum);
